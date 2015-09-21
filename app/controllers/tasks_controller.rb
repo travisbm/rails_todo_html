1e3abaeff02db8598df80ce298bc47e9398ca548
Travis Montgomery <travisbm@gmail.com>
2015-09-21 16:03:30 -0500

    added index and show methods to tasks_controller. added header to application.html.erb. wrote html in index.html.erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,19 @@
 class TasksController < ApplicationController
   def index
+    tasks = Task.all
+
+    respond_to do |f|
+      f.html do
+        render template: "tasks/index.html.erb", locals:  { all_tasks: tasks }
+      end
+      f.json do
+        render json: tasks.to_json, status: 200
   end
 
   def show
+    task = Task.find(params[:id])
+
+    render template: "tasks/show.html.erb", locals: { task: task }
   end
 
   def create
